081041b612cb5bd93b6618d542ca93d941c888b4
Eugene Oleshkevich <jackie07364538@gmail.com>
2014-05-26 23:08:29 +0300

    add tasks 11, 12

---
+++ tasks/tasks11.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Найти в двумерном массиве сумму элементов в тех столбцах, в которых есть хотя бы 1 отрицательный элемент
+namespace Task2
+{
+
+    class task
+    {
+
+        static void Main()
+        {
+            int column = 3, str = 5;
+            int[] sumNegativeValue;
+
+            matrix Matrix = new matrix(column, str);
+            Matrix.randomValueMatrix();
+            Matrix.showMatrix();
+            sumNegativeValue = Matrix.searchNegativeValue();
+            showArraySumNegativeValue(sumNegativeValue);
+            Console.ReadKey(true);
+        }
+
+        static public void showArraySumNegativeValue(int[] sumArray)
+        {
+            Console.WriteLine("Сумма элементов в тех столбцах, в которых есть хотя бы 1 отрицательный элемент:");
+            foreach (int x in sumArray)
+            {
+                Console.Write(x + "\t");
+            }
+            Console.WriteLine();
+        }
+       #region matrix
+        public class matrix
+        {
+            private int column; 
+            private int str;
+
+            private int[,] Matrix;
+
+            public matrix()
+            {
+                column = 0;
+                str = 0;
+                Matrix = new int[str, column];
+            }
+
+            public matrix(int str_new, int column_new)
+            {
+                
+                if (str_new < 0 || column_new < 0)
+                    column = str = 0;
+
+                str = str_new;
+                column = column_new;
+                Matrix = new int[str, column];
+            }
+            public void showMatrix()
+            {
+                
+
+                for (int i = 0; i < str; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                        Console.Write(Matrix[i, j] + "\t");
+                    Console.WriteLine();
+                }
+
+            }
+
+            public void randomValueMatrix()
+            {
+                Random random = new Random();
+                for (int i = 0; i < str; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                        Matrix[i, j] = random.Next(-2, 18);
+                }
+            }
+
+            public int[] searchNegativeValue()
+            {
+                int[] sumNegativrValue = new int[column];
+                int indexSum = 0;
+                bool flag = false;
+
+                for (int j = 0; j < column; j++)
+                {
+                    for (int i = 0; i < str; i++)
+                    {
+                        if (Matrix[i, j] < 0)
+                        {
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    if (flag == true)
+                    {
+                        for (int x = 0; x < str; x++)
+                        {
+                            sumNegativrValue[indexSum] += Matrix[x, j];
+                        }
+
+                        flag = false;
+
+                    }
+
+                    indexSum++;
+                }
+                return sumNegativrValue;
+            }
+
+        }
+       #endregion matrix
+
+    }
+}
---
+++ tasks/tasks12.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Проверить, все ли строки матрицы упорядочены по убыванию. Если нет, найти первую неупорядоченную строку и упорядочить.
+namespace Task2
+{
+
+    class task
+    {
+        static void Main()
+        {
+            int column = 3, str = 5;
+            matrix Matrix = new matrix(str, column);
+            Matrix.randomValue();
+            Matrix.showMatrix();
+            Matrix.sortMatrix();
+            Console.WriteLine("---Sort--");
+            Matrix.showMatrix();
+            Console.ReadKey(true);
+        }
+
+        #region matrix
+        public class matrix
+        {
+            private int str;
+            private int column;
+            private int[][] Matrix;
+
+            public matrix()
+            {
+                str = 0;
+                column = 0;
+
+                Matrix = new int[str][];
+                for (int i = 0; i < str; i++)
+                {
+                    Matrix[i] = new int[column];
+                }
+            }
+            public matrix(int str_new, int column_new)
+            {
+                if (str_new < 0)
+                    str_new = 0;
+                if (column < 0)
+                    column_new = 0;
+
+                str = str_new;
+                column = column_new;
+
+                Matrix = new int[str][];
+                for (int i = 0; i < str; i++)
+                {
+                    Matrix[i] = new int[column];
+                }
+            }
+            public void randomValue()
+            {
+                Random random = new Random();
+                for (int i = 0; i < str; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        Matrix[i][j] = random.Next(0, 10);
+                    }
+                }
+            }
+            public void showMatrix()
+            {
+                for (int i = 0; i < str; i++)
+                {
+
+                    for (int j = 0; j < column; j++)
+                    {
+                        Console.Write(Matrix[i][j] + " ");
+                    }
+                    Console.WriteLine();
+
+                    
+                }
+            }
+            public void sortMatrix()
+            {
+                bool flagNotSort = false;
+                int notSort;
+                int pastArrayValue;
+
+                for (notSort = 0; notSort < str; notSort++)
+                {
+                    pastArrayValue = Matrix[notSort][0];
+                    for (int j = 1; j < column; j++)
+                    {
+                        if (pastArrayValue > Matrix[notSort][ j])
+                        {
+                            flagNotSort = true;
+                            break;
+                        }
+                    }
+                    if (flagNotSort == true)
+                    {
+                        break;
+                    }
+                }
+                if (flagNotSort == true)
+                {
+                    Array.Sort(Matrix[notSort]);
+                }
+                //for (int i = 0; i < str; i++)
+                //{
+                //    Array.Sort(Matrix[i]);
+                //}
+            }
+        }
+        #endregion matrix
+
+    }
+}
