8528015cb3a1f051789f90321d74036a199b11ce
Eugene Oleshkevich <jackie07364538@gmail.com>
2014-05-29 22:43:56 +0300

    add new theory: Singleton, Destructor,IDisposable

---
+++ theoryAndPractice/Prog10_Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Класс Singleton. Особенностью этого класса является то, что в при-
+ложении можно создать только один объект данного класса.
+ */
+namespace PracticCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton A = Singleton.Create();
+            Singleton D = Singleton.Create();
+
+            A.info = "information";
+            Console.WriteLine(D.info);
+           
+            Console.ReadKey(true);
+        }
+        class Singleton
+        {
+            static Singleton Instance;      //поле Instance хранит ссылку на объект и описано как статическое.
+                                            // Это сделано для того, чтобы с ним
+                                            //можно было работать в статическом методе Create().
+            public string info;
+            private Singleton() { }
+
+            public static Singleton Create()            //Create() всегда возвращает ссылку на один и тот же объект.
+            {
+                if (Instance == null) Instance = new Singleton();
+                return Instance;
+            }
+
+        }
+    }
+}
---
+++ theoryAndPractice/Prog11_DetructorAndIDisposable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Интерфейс IDisposable, содержащий единственный метод Dispose(), куда
+помещается завершающий код работы с объектом. Класс, объекты которого
+требуется освобождать «вручную», реализовывает интерфейс IDisposable
+ */
+namespace PracticCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //C# имеет специальную обрамляющую конструкцию using, которая гаран-
+            //тирует вызов метода Dispose() для объектов, использующихся в своем блоке.
+            using (classWithDestructor A = new classWithDestructor("A"))
+            {
+                A.doSomething();
+                // компилятор поместит сюда вызов A.Dispose()
+            }
+            Console.ReadKey(true);
+        }
+
+        class classWithDestructor : IDisposable
+        {
+            public string name;
+
+            public classWithDestructor(string name)
+            {
+                this.name = name;
+            }
+
+            public void doSomething()
+            {
+                Console.WriteLine("I'm working!");
+            }
+            //реализуем метод освобождения
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose called for " + name);
+            }
+
+            ~classWithDestructor()
+            {
+                //lдеструктор вызывает Dispose На всякий случай
+                Dispose();
+                Console.WriteLine("Bye!");
+            }
+        }
+    }
+}
