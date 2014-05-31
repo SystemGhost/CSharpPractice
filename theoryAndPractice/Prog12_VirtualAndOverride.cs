2483658473d17c2410274617c9e0fd8db5969772
Eugene Oleshkevich <jackie07364538@gmail.com>
2014-05-31 22:29:02 +0300

    add new theory: Virtual, Override

---
+++ theoryAndPractice/Prog12_VirtualAndOverride.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Для организации полиморфного вызова методов применяется пара ключе-
+вых слов virtual и override: virtual указывается для метода базового клас-
+са, который мы хотим сделать полиморфным, override – для методов произ-
+43
+водных классов. Эти методы должны совпадать по имени и сигнатуре с пере-
+крываемым методом класса-предка.
+ */
+namespace PracticCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            CPet cpet, cat;
+            cpet = new CPet();
+            cat = new Cat();
+            cpet.Speak();
+            cat.Speak();
+            Console.ReadKey(true);
+        }
+
+        class CPet
+        {
+            public virtual void Speak()
+            {
+                Console.WriteLine("I'm Pet!");
+            }
+        }
+
+        class Cat : CPet
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("I'm Cat!");
+            }
+        }
+        
+    }
+}
