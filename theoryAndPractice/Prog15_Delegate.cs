754a415bbdec170d947862abc1625071f54f5ac6
Eugene Oleshkevich <jackie07364538@gmail.com>
2014-06-06 22:29:38 +0300

    add new theory: Delegate

---
+++ theoryAndPractice/Prog15_Delegate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *  Делегаты
+ */
+namespace PracticCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] A = { 1, 20, 30 };
+            PrintMethod first, second, result;
+            first = new PrintMethod(MainClass.ConsolePrint);
+            MainClass C = new MainClass();
+            second = new PrintMethod(C.FormatPrint);
+            result = first + second;
+            ArrayPrint.print(A, result);
+
+            Console.ReadKey(true);
+        }
+        delegate void PrintMethod(int x);
+
+        class ArrayPrint
+        {
+            public static void print(int[] A, PrintMethod P) {
+                foreach(int element in A)
+                P(element);
+                }
+        }
+
+        class MainClass
+        {
+            public static void ConsolePrint(int i)
+            {
+                Console.WriteLine(i);
+            }
+            public void FormatPrint(int i)
+            {
+                Console.WriteLine("Element is {0}", i);
+            }
+            
+        }
+    }      
+}
